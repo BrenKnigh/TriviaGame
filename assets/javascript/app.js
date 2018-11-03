867f37cb9b42f129fb129643a50a19bc40881c9e
WaffleCode <brenknigh117@gmail.com>
2018-11-03 10:32:57 -0700

    Answer THESE!

---
+++ assets/javascript/app.js
@@ -0,0 +1,136 @@
+var panel = $("#play-area");
+
+$(document).on("click", "#start", function(event){
+  game.start();
+});
+
+$(document).on("click", "#done", function(event){
+  game.done();
+});
+
+var questions = [{
+	question: "How many Seasons have there been as of 2018?",
+	choices: ["6", "5", "8", "7"],
+	correctAnswer: "8"
+
+}, {	
+
+	question: "What Champion is Obsessed with Bombs?",
+	choices: ["Ziggs", "Teemo", "Lulu", "Veigar"],
+	correctAnswer: "Ziggs"
+
+}, {
+
+	question: "Who on Release, was widly considered to be the most broken champion?",
+	choices: ["Master Yi", "Rek'sai", "Xin Xhao", "Katarina"],
+	correctAnswer: "Xin Xhao"
+
+}, {
+
+	question: "What year did Riot officially add ARAM as a game mode?",
+	choices: ["2015", "2013", "2014", "2012"],
+	correctAnswer: "2012"
+
+}, {
+
+	question: "Who in the Leauge is best known for Dunking?",
+	choices: ["Garen", "Darius", "Shen", "Pyke"],
+	correctAnswer: "Darius"
+
+}];
+
+var game = {
+	correct: 0,
+	incorrect: 0,
+	counter: 60,
+
+  countdown: function() {
+  	game.counter--;
+  	$("#counter-number").html(game.counter);
+
+  	if (game.counter === 0) {
+  		alert("TIME'S UP");
+  		game.done();
+
+  	}
+  },
+
+  start: function() {
+  	timer = setInterval(game.countdown, 1000);
+  	$('#subcontainer').prepend('<h2>Time Remaining: <span id="counter-number">60</span> Seconds</h2>');
+  	$("#start").remove();
+
+  	for (var i = 0; i < questions.length; i++) {
+      panel.append('<h2>' + questions[i].question + '</h2>');
+      for (var j = 0; j < questions[i].choices.length; j++){
+        panel.append('<input type="radio" name ="question' + '-' + i + '"value="' + questions[i].choices[j] + '">' + questions[i].choices[j]);
+        }
+  		}
+  		panel.append("<button id='done'>DONE</button>");
+      
+  	},
+
+  	done: function() {
+
+  		$.each($("input[name='question-0']:checked"), function() {
+  			if ($(this).val() == questions[0].correctAnswer) {
+  				console.log(this);
+  				  game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+  		$.each($("input[name='question-1']:checked"), function() {
+  			if ($(this).val() == questions[1].correctAnswer) {
+  				console.log(this);
+  				game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+  		$.each($("input[name='question-2']:checked"), function() {
+  			if ($(this).val() == questions[2].correctAnswer) {
+  				console.log(this);
+  				game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+  		$.each($("input[name='question-3']:checked"), function() {
+  			if ($(this).val() == questions[3].correctAnswer) {
+  				console.log(this);
+  				game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+  		$.each($("input[name='question-4']:checked"), function() {
+  			if ($(this).val() == questions[4].correctAnswer) {
+  				console.log(this);
+  				game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+
+  		this.results();
+  	},
+
+
+  	  results:function() {
+  	  	clearInterval(timer);
+
+  	  	$("#subcontainer h2").remove();
+  	   panel.html("<h2>You're Done!</h2>");
+  	   panel.append("<h3>You got this many right: " + this.correct + "</h3>");
+  	   panel.append("<h3>You got this many wrong: " + this.incorrect + "</h3>");
+  	   panel.append("<h3>You didn't answer this many: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  	  
+  	  }
+
+  };
